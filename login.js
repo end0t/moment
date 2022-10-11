c4757996e7b04db6ecc667741d505d868d5ec04e
seogh <gcon8431@gmail.com>
2022-10-11 16:56:02 +0900

    second commit

---
+++ login.js
@@ -0,0 +1,5 @@
+(function(){
+    const loginForm = document.querySelector("#login-form");
+    const loginInput = loginForm.querySelector("input");
+    const loginBtn = loginForm.querySelector("button");
+})();
